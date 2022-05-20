96e6aa2bada94395612de336547566333ed45a36
KzZe-Sama <lilmisstreebox@gmail.com>
2022-05-20 11:17:19 -0400

    added pets database

--- flaskr/db.py
+++ flaskr/db.py
@@ -1,7 +1,10 @@
 from flask_sqlalchemy import SQLAlchemy
-def db(app): 
-    if app is not None:
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://ice:hello@localhost/pets'
-        db = SQLAlchemy(app)
-        return db
-    return None
+from app import app
+from models.PetsModel import Pet
+# setup
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://ice:hello@localhost/pets'
+db = SQLAlchemy(app)
+
+
+# create tables
+db.create_all()
---
+++ flaskr/models/PetsModel.py
@@ -0,0 +1,12 @@
+from db import db
+
+class Pet(db.Model):
+    __tablename__ = 'pets'
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(100), nullable = False)
+    pet_type = db.Column(db.String(100), nullable = False)
+    pet_age = db.Column(db.Integer(), nullable = False)
+    pet_description = db.Column(db.String(100), nullable = False)
+
+    def __repr__(self):
+        return f"Pet - {self.name}"
