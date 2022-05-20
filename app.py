e87b75a3b0e858d4164bf59de399b36b684e6c73
KzZe-Sama <lilmisstreebox@gmail.com>
2022-05-20 11:48:28 -0400

    flask server setup routes + 1

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ import imp
 from flask import Flask,Blueprint
 from flaskr.db import setup_db
 from flaskr.routes.route import main
+from flask-cors import CORS
+
 
 app = Flask(__name__)
 app.register_blueprint(main)
-
+CORS(app)
 # calling db
 setup_db(app)
 if __name__ == '__main__':
--- flaskr/routes/route.py
+++ flaskr/routes/route.py
@@ -3,6 +3,7 @@ from flask import Blueprint,jsonify
 
 main = Blueprint('main',__name__)
 
+@cross-origin()
 @main.route('/')
 def main_index():
     return jsonify({"message":"List of pets"})
